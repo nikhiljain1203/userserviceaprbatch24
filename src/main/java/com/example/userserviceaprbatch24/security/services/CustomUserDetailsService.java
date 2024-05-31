7e8fb33f8064f9457aee88fff72ba880fd09016b
apple <nikhiljain1203@gmail.com>
2024-05-31 23:14:45 +0530

    OAuth

---
+++ src/main/java/com/example/userserviceaprbatch24/security/services/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.userserviceaprbatch24.security.services;
+
+import com.example.userserviceaprbatch24.models.User;
+import com.example.userserviceaprbatch24.repos.UserRepo;
+import com.example.userserviceaprbatch24.security.models.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    UserRepo userRepo;
+
+    public CustomUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepo.findByEmail(username);
+        if(optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with email: " + username);
+        }
+        return new CustomUserDetails(optionalUser.get());
+    }
+}
